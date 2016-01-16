e4fbb594b12815c6850e0105cd47af5cfc9db194
olitreadwell <olitreadwell@gmail.com>
2016-01-15 18:12:36 -0600

    include requirements

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+
+// set up =========================================
+var express        = require('express');
+var app            = express();
+var mongoose       = require('mongoose');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+// configuration ==================================
+
+// listen (start app with node server.js) =========
