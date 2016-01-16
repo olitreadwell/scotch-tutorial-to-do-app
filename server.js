b749609a7248ca2ba5fd9d5c53c0c31e3fe6178c
olitreadwell <olitreadwell@gmail.com>
2016-01-15 18:21:38 -0600

    include configuration info

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 
-// set up =========================================
+// set up ======================================================
 var express        = require('express');
 var app            = express();
 var mongoose       = require('mongoose');
@@ -8,6 +8,16 @@ var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
-// configuration ==================================
+// configuration ===============================================
 
-// listen (start app with node server.js) =========
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+
+// listen (start app with node server.js) ======================
