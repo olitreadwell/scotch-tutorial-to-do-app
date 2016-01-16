70ce3fbc45a592f0f0c2d9a2ad3d7506f062e66a
olitreadwell <olitreadwell@gmail.com>
2016-01-15 18:22:31 -0600

    include port info for server

--- server.js
+++ server.js
@@ -19,5 +19,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-
 // listen (start app with node server.js) ======================
+app.listen(8080);
+console.log("App listening on port 8080");
