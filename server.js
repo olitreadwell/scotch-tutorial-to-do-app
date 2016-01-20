6b30e3784e789c3f50cfb050498dd4181c1f3fd0
olitreadwell <olitreadwell@gmail.com>
2016-01-19 18:11:54 -0600

    add todo model

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
+// define model ================================================
+
+var Todo = mongoose.model('Todo', {
+  text : String
+});
+
 // listen (start app with node server.js) ======================
 app.listen(8080);
 console.log("App listening on port 8080");
