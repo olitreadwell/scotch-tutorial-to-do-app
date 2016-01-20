efce4face74569184479931247e62e76d7e17e54
olitreadwell <olitreadwell@gmail.com>
2016-01-19 23:22:12 -0600

    add new todo to application

--- public/core.js
+++ public/core.js
@@ -14,5 +14,18 @@ function mainController($scope, $http) {
       console.log('Error: ' + data);
     });
 
+// when submitting the add form, send the text to the node API
+$scope.createTodo = function() {
+  $http.post('/api/todos', $scope.formData)
+    .success(function(data) {
+      $scope.formData = {};
+      // clear the form so our user is ready to enter another
+      $scope.todos = data;
+      console.log(data);
+    })
+    .error(function(data) {
+      console.log('Error; ' + data);
+    });
+};
 
-}
+
