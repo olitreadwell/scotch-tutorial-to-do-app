8d713b7133d08304569d1dd5f1f3f23a5a7524e6
olitreadwell <olitreadwell@gmail.com>
2016-03-11 21:33:22 -0600

    pushing to heroku with my own mongodb still doesn't work :(

--- public/core.js
+++ public/core.js
@@ -8,7 +8,7 @@ function mainController($scope, $http) {
   $http.get('/api/todos')
     .success(function(data) {
       $scope.todos = data;
-      // console.log(data);
+      console.log(data);
 
     })
     .error(function(data) {
@@ -22,7 +22,7 @@ function mainController($scope, $http) {
       .success(function(data) {
         $scope.formData = {}; // clear the form so our user is ready to enter another
         $scope.todos = data;
-        // console.log(data);
+        console.log(data);
 
       })
       .error(function(data) {
